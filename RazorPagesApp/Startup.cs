dd6178cfa0106d8d88672f7952ece986d668451e
Jimmy Javier <jimmyjavier@hotmail.fr>
2020-07-13 10:01:08 -0500

    Agregado Modelo y primer CRUD

--- RazorPagesApp/Startup.cs
+++ RazorPagesApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesApp.Data;
 
 namespace RazorPagesApp
 {
@@ -24,6 +26,9 @@ namespace RazorPagesApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<RazorPagesAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorPagesAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
